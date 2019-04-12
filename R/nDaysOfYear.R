3b8218cd2d782e2122a1e006b639305bcd649d55
neumannd <daniel.neumann@io-warnemuende.de>
2019-04-12 15:35:38 +0200

    renamed number-of-days-of-year function

--- R/nDaysOfYear.R
+++ R/nDaysOfYear.R
@@ -10,13 +10,13 @@
 #' @examples
 #' 
 #' # a non-leap year:
-#' ndays_of_year(2001)
+#' nDaysOfYear(2001)
 #' > 365
 #' 
 #' # a leap year:
-#' ndays_of_year(2004)
+#' nDaysOfYear(2004)
 #' > 366
-ndays_of_year = function(year) {
+nDaysOfYear = function(year) {
   nDays = 365
   if (is.leapyear(year)) nDays = 366
   return(nDays)
