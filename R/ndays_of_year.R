5352dc434bb614f54496cd90a4af4910cdc32aef
neumannd <daniel.neumann@io-warnemuende.de>
2019-04-12 15:31:27 +0200

    documentation of day-of-year function

--- R/ndays_of_year.R
+++ R/ndays_of_year.R
@@ -1,3 +1,21 @@
+#' Get number of days of provided year
+#' 
+#' Return the number of days of the year provided as input argument
+#'
+#' @param year numeric; a year
+#'
+#' @return numeric: numer of days of the particular year
+#' @export
+#'
+#' @examples
+#' 
+#' # a non-leap year:
+#' ndays_of_year(2001)
+#' > 365
+#' 
+#' # a leap year:
+#' ndays_of_year(2004)
+#' > 366
 ndays_of_year = function(year) {
   nDays = 365
   if (is.leapyear(year)) nDays = 366
